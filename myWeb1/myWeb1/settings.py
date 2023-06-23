d4fcee0e9fada99dc7968f936ca2ed0b8aad3eb2
Moisés Jurado <moises003@gmail.com>
2023-06-23 13:33:33 -0500

    tutorial07 - 8 Customizing your project’s templates

--- myWeb1/myWeb1/settings.py
+++ myWeb1/myWeb1/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "myWeb1.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
